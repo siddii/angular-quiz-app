bd0a92e78e1d6265f457bec17c8a1e9e03a18181
Siddique Hameed <s.hameed@elsevier.com>
2013-04-16 09:56:05 -0500

    Got timer directive working

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,11 +1,36 @@
 'use strict';
 
 /* Directives */
+angular.module('quiz-app.directives', [])
+  .directive('timer', function($timeout, dateFilter) {
+    return  {
+      restrict: 'E',
+      templateUrl: 'templates/timer.html',
+      replace: true,
+      link: function(scope, element, attrs) {
+      var startTime = new Date();
+      var format = "m 'minute(s)', s 'seconds'",  // date format
+        timeoutId;
 
+      function updateTime() {
+        var timeElapsed = new Date() - startTime;
+        //element.text(dateFilter(timeElapsed, format));
+        scope.timeTaken = dateFilter(timeElapsed, format);
+      }
 
-angular.module('quiz-app.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+      function updateLater() {
+        timeoutId = $timeout(function() {
+          updateTime();
+          updateLater();
+        }, 1000);
+      }
+
+      element.bind('$destroy', function() {
+        $timeout.cancel(timeoutId);
+      });
+
+      updateTime();
+      updateLater();
+    }
+  }
+});
